2ddeaabb6645a12c593386bb988fbc89c0849f27
Harshit Trivedi <u6101164@anu.edu.au>
2017-04-02 12:04:36 +1000

    Deleting old files and adding new template

--- js/main.js
+++ js/main.js
@@ -1,71 +1,136 @@
-d3.csv("data/final-data.csv", function(d) {
-  var format = d3.time.format("%Y");
-  return {
-    'Year': format.parse(d.year),
-    'Carrier Name': d.carrier_name,
-    'On Time': +d.on_time,
-    'Arrivals': +d.arrivals
-  };
-}, function(data) {
-  'use strict';
+(function ($) {
+   "use strict";
 
-  // append title
-  d3.select('#content')
-    .append('h2')
-    .attr('id', 'title')
-    .text('On-Time Arrival Rates for Top U.S. Domestic Airlines, 2003-2013');
 
-  // set svg
-  var width = 980,
-	height = 550;
-  var svg = dimple.newSvg('#content', width, height);
-  var myChart = new dimple.chart(svg, data);
+/*----------------------------
+ wow js active
+------------------------------ */
+	new WOW().init();   
 
-  // set y axis
-  var minY = 0.5,
-      maxY = 1;
-  var y = myChart.addMeasureAxis('y', 'On Time');
-  y.tickFormat = '%';
-  y.overrideMin = minY;
-  y.overrideMax = maxY;
-  y.title = 'Percentage of Arrivals on Time (within 15 minutes)';
 
-  // set x axis
-  var x = myChart.addTimeAxis('x', 'Year');
-  x.tickFormat = '%Y';
-  x.title = 'Year';
+/*---------------------
+ TOP Menu Stick
+--------------------- */
+	var s = $("#sticker");
+	var pos = s.position();					   
+	$(window).scroll(function() {
+		var windowpos = $(window).scrollTop();
+		if (windowpos > pos.top) {
+		s.addClass("stick");
+		} else {
+			s.removeClass("stick");	
+		}
+	});
+/*----------------------------
+ Navbar nav
+------------------------------ */
 
-  // set series and legend
-  var s = myChart.addSeries('Carrier Name', dimple.plot.scatter);
-  var p = myChart.addSeries('Carrier Name', dimple.plot.line);
-  var legend = myChart.addLegend(width*0.65, 60, width*0.25, 60, 'right');
+	$(".main-menu ul.navbar-nav li ").on('click', function(){
+		$(".main-menu ul.navbar-nav li").removeClass("active");
+		$(this).addClass("active");
+	});	
 
-  // draw
-  myChart.draw();
+/*----------------------------
+ Header Images
+------------------------------ */
+	$(".header-img").height($(window).height());
 
-  // handle mouse events on gridlines
-  y.gridlineShapes.selectAll('line')
-    .style('opacity', 0.25)
-    .on('mouseover', function(e) {
-      d3.select(this)
-        .style('opacity', 1);
-    }).on('mouseleave', function(e) {
-      d3.select(this)
-        .style('opacity', 0.25);
-    });
+	$(window).resize(function(){
+		$(".header-img").height($(window).height());
+	});	
 
-  // handle mouse events on paths
-  d3.selectAll('path')
-    .style('opacity', 0.25)
-    .on('mouseover', function(e) {
-      d3.select(this)
-        .style('stroke-width', '8px')
-        .style('opacity', 1)
-        .attr('z-index', '1');
-  }).on('mouseleave', function(e) {
-      d3.select(this)
-        .style('stroke-width', '2px')
-        .style('opacity', 0.25)
-        .attr('z-index', '0');
-  });
-});
+/*----------------------------
+Page Scroll
+------------------------------ */
+
+	jQuery('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		  $('html, body').stop().animate({
+			  scrollTop: $($anchor.attr('href')).offset().top - 55
+			}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	}); 
+
+/*----------------------------
+ Counter js active
+------------------------------ */
+
+	$('.counter').counterUp({
+		delay: 40,
+		time: 3000
+	});
+/*---------------------
+ mixItUp
+--------------------- */	
+
+	$('.awesome-portfolio-content').mixItUp({
+		animation: {
+		  effects: 'rotateZ',
+		  duration: 1000,
+		}
+	});
+
+/*---------------------
+ testimonial-curosel
+--------------------- */
+	$('.testimonial-carousel').owlCarousel({
+		loop:true,
+		margin:0,
+		nav:true,
+		dots:false,
+		animateOut: 'slideOutUp',
+		animateIn: 'zoomInRight',
+		navText:["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+		autoplay:false,
+		smartSpeed:3000,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
+	});
+
+/*---------------------
+ Blog-curosel
+--------------------- */
+	$('.blog-item ').owlCarousel({
+		loop:true,
+		margin:0,
+		nav:true,
+		dots:false,
+		animateOut: 'slideOutDown',
+		animateIn: 'zoomInLeft',
+		navText:["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+		autoplay:false,
+		smartSpeed:3000,
+		responsive:{
+			0:{
+				items:1
+			},
+			768:{
+				items:2
+			},
+			1000:{
+				items:3
+			}
+		}
+	});
+
+/*--------------------------
+ scrollUp
+---------------------------- */
+	$.scrollUp({
+		scrollText: '<i class="fa fa-angle-up"></i>',
+		easingType: 'linear',
+		scrollSpeed: 900,
+		animation: 'fade'
+	});
+	
+
+})(jQuery); 
